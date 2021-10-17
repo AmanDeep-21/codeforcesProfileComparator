76cf520f2a1ce7900a219eb23d6674e6d5177054
Puchalla Surya Prakash Reddy <surya_prakashreddy@yahoo.com>
2021-10-17 13:24:27 +0530

    Added Social Media Profiles in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,13 +15,21 @@ class Footer extends React.Component{
             width : 1500
         }
 
+        let socialMediaLinksStyle = {
+            marginRight: '8px',
+        }
+
         return(
             <div style = {myFooterStyle}>
                 <hr />
                 <center>
                     <p>Copyright &copy; {(new Date().getFullYear())}</p>
                     <p>Created by Aman Deep</p>
-                    <p>StopStalk Profile : <a href = "https://www.stopstalk.com/user/profile/aman_deep21" target = "_blank" rel="noopener noreferrer" id = "stopstalkLink">aman_deep21</a></p>
+                    <p>Follow me on: 
+                        <span style={socialMediaLinksStyle}><a href = "https://www.stopstalk.com/user/profile/aman_deep21" target = "_blank" rel="noopener noreferrer">StopStalk</a></span>
+                        <span style={socialMediaLinksStyle}><a href = "https://www.linkedin.com/in/amandeep21" target = "_blank" rel="noopener noreferrer">LinkedIn</a></span>
+                        <span style={socialMediaLinksStyle}><a href = "https://github.com/AmanDeep-21" target = "_blank" rel="noopener noreferrer">GitHub</a></span>
+                    </p>
                 </center>
             </div>
         )
